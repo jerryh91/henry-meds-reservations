551e83b86c36de8d80b8c45c8826550bc430dc98
Jerry Hou <jerryhou91@gmail.com>
2024-04-17 17:58:34 -0500

    a

--- src/main/java/com/henrymedstakehome/reservation/services/ScheduleService.java
+++ src/main/java/com/henrymedstakehome/reservation/services/ScheduleService.java
@@ -1,7 +1,9 @@
 package com.henrymedstakehome.reservation.services;
 
 import java.time.ZonedDateTime;
-import java.util.TreeSet;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
@@ -13,14 +15,14 @@ import com.henrymedstakehome.reservation.models.ProviderTimeslot;
 public class ScheduleService {
     //Storing timeslots in local memory due to time
     //In production will connect with an external db through ORM like hibernate
-    TreeSet<ProviderTimeslot> timeslots = new TreeSet<>();
+    List<ProviderTimeslot> timeslots = new ArrayList<>();
 
     //Assume: overwrites all existing timeslots for providerId.
     public void replaceTimeslots(final ProviderAvailability providerAvailability) {
         //create 15 min block of time between startTime and endTime inclusive
         //remove all existing provider timeslots
-        this.timeslots = timeslots.stream().filter(t -> t.getProvider().equals(providerAvailability.getProvider())).collect(Collectors 
-                            .toCollection(TreeSet::new));
+        this.timeslots = timeslots.stream().filter(t -> !t.getProvider().equals(providerAvailability.getProvider())).collect(Collectors 
+                            .toCollection(ArrayList::new));
         ZonedDateTime currStartTime = providerAvailability.getStartDateTime();
         ZonedDateTime endTime = providerAvailability.getEndDateTime();
         while (currStartTime.plusMinutes(15).isEqual(endTime) ||
@@ -28,6 +30,8 @@ public class ScheduleService {
             this.timeslots.add(new ProviderTimeslot(providerAvailability.getProvider(), currStartTime, null));
             currStartTime = currStartTime.plusMinutes(15);
         }
+        Collections.sort(timeslots);
+        // System.out.println("finished add timeslots");
     }
     
     // public List<ZonedDateTime> getTimeslots() {
