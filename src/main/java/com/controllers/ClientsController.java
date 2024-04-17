779f3b045d47bb1d1c42215e423b12cb4cad1228
Jerry Hou <jerryhou91@gmail.com>
2024-04-17 13:01:28 -0500

    a

--- src/main/java/com/controllers/ClientsController.java
+++ src/main/java/com/controllers/ClientsController.java
@@ -3,8 +3,16 @@ package com.controllers;
 
 // - Allows a client to retrieve a list of available appointment slots
 //     - Appointment slots are 15 minutes long
-// - Allows clients to reserve an available appointment slot
-// - Allows clients to confirm their reservation
+//default: next 5 days 
+
+// - Allows clients to reserve an available appointment slot: 
+    //
+// - Allows clients to confirm their reservation: remove from timeslot map
+
+// Additional Requirements:
+
+// - Reservations expire after 30 minutes if not confirmed and are again available for other clients to reserve that appointment slot
+// - Reservations must be made at least 24 hours in advance
 public class ClientsController {
     
 }
--- src/main/java/com/controllers/ProvidersController.java
+++ src/main/java/com/controllers/ProvidersController.java
@@ -1,13 +1,26 @@
 package com.controllers;
 
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.models.Availability;
+import com.services.ScheduleService;
+
 @RestController(value ="/api-reservations/v3/providers")
 public class ProvidersController {
+    
     
     @Autowired
     private ScheduleService scheduleService;
@@ -15,8 +28,20 @@ public class ProvidersController {
 //     - Allows providers to submit times they are available for appointments
 //     - e.g. On Friday the 13th of August, Dr. Jekyll wants to work between 8am and 3pm
     @PostMapping(value = "/{providerId}/timeslots", consumes = "application/json")
-    public void postTimes(@PathVariable String providerId, @RequestBody List<> timeslots) {
+    public ResponseEntity<String> postTimes(@PathVariable String providerId, @RequestBody Availability availability) {
+            //edge cases: 
+            if (!availability.isValid()) return ResponseEntity.badRequest().body("StartTime or endTime provided is empty");
+            //if less than 15 mins between end - start 
+            if (ChronoUnit.MINUTES.between(availability.getStartDateTime(), availability.getEndDateTime()) < 15) {
+                return ResponseEntity.badRequest().body("EndTime is 15 minutes less than StartTime");
+            }
+            //Convert each UTC time to ensure date is in UTC
+            availability.setEndDateTime(availability.getEndDateTime().withZoneSameInstant(ZoneId.of("UTC")));
+            availability.setStartDateTime(availability.getStartDateTime().withZoneSameInstant(ZoneId.of("UTC")));
+            //call scheduleService to persist available time slots 
+            scheduleService.replaceTimeslots(providerId, availability);
 
+            return ResponseEntity.ok().body("Success");
     } 
 
 
